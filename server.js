6f1e5297e8d276a3f24deb11912cd7f9e362c69a
Subratadey <subratadey46@gmail.com>
2016-10-22 18:25:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Science', function(req,res){
+    res.send('Science Course one will be served here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
